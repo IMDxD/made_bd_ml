76045d6e7471dd07a8f2273d97aca4b274417607
IMDxD <nestor969@mail.ru>
2021-09-30 19:10:44 +0300

    fix mapper.py

--- hw1/mapper.py
+++ hw1/mapper.py
@@ -1,8 +1,17 @@
 import csv
 import sys
 
+total_count = 0
+total_sum = 0
+total_square_sum = 0
 for row in csv.reader(sys.stdin, delimiter=","):
     if len(row) > 8:
         price = row[9]
         if price != "price":
-            print('{0}\t0\t1'.format(price))
+            price = float(price)
+            total_sum += price
+            total_square_sum += price ** 2
+            total_count += 1
+total_mean = total_sum / total_count
+total_variance = total_square_sum / total_count - total_mean ** 2
+print('{0}\t{1}\t{2}'.format(total_mean, total_variance, total_count))
