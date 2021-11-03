ecd14a7928605beb3deafe1aa0e02917f30a276f
IMDxD <nestor969@mail.ru>
2021-11-03 17:20:22 +0300

    fix tf computation

--- hw4/src/main/scala/Main.scala
+++ hw4/src/main/scala/Main.scala
@@ -38,17 +38,29 @@ object Main {
       .agg(countDistinct(col("row")).as("WordDocuments"))
       .withColumn("DocumentCount", lit(df.count()))
       .orderBy(desc("WordDocuments")).limit(100)
+    val dfDocLen = dfExploded
+      .groupBy("row")
+      .agg(sum(col("WordCount")).as("DocLen"))
+    val dfWordCountDocLen = dfDocLen.as("left")
+      .join(dfExploded.as("right"), dfDocLen("row") === dfExploded("row"))
+      .select(
+        col("left.row"),
+        col("left.DocLen"),
+        col("right.Word"),
+        col("right.WordCount")
+      )
+    val dfTf = dfWordCountDocLen.withColumn("Tf", col("WordCount") / col("DocLen"))
     val dfIdf = dfWithDocCount
       .withColumn("Idf", log(
         col("DocumentCount") / col("WordDocuments")
       ))
       .select(col("Word"), col("Idf"))
-    val dfJoined =dfExploded.as("left")
+    val dfJoined = dfTf.as("left")
       .join(dfIdf.as("right"), dfExploded("Word") === dfIdf("Word"))
       .select(
         col("row"),
         col("left.Word"),
-        col("left.WordCount").as("Tf"),
+        col("left.Tf"),
         col("right.Idf").as("Idf")
       )
     val dfTfIdf = dfJoined.withColumn("TfIdf", col("Tf") * col("Idf"))
